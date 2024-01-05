17226b9f281a8ac9dee7e6dce0baad917ce5d713
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2024-01-05 15:11:22 +0800

    Add files via upload

---
+++ Lab Activity 1 Components Demo/hello.jsx
@@ -0,0 +1,11 @@
+export default function Hello() {
+    var message = "I think i've done it"
+    return(
+        <>
+        <div>
+            <p>Hello World</p>
+            <h1>{message}</h1>
+        </div>
+        </>
+    );
+}
