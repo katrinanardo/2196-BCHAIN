ec3b64874fa9f69efbf67e5d6f6da4c285d15a4a
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2024-01-12 15:23:40 +0800

    Add files via upload

---
+++ Conditional-Demo-4/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './components/hello'
+import Welcome from './components/welcome'
+import BookList from './components/BookList'
+import ChoreList from './components/ChoresList'
+import MovieList from './components/MovieList'
+import Greeting from './components/Greetings'
+import { ArrowGreeting } from './components/Arrow Greeting'
+import ShinyButton from './components/ShinyButton'
+import Appp from './components/demo4'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    < Appp />
+    </>
+  )
+}
+
+export default App
---
+++ Conditional-Demo-4/demo4.jsx
@@ -0,0 +1,27 @@
+import classes from './demo3.module.css'
+
+function Red(){
+    return(<h1>Stop the car!</h1>
+    );
+}
+
+function Green(){
+    return(<h1>Go !</h1>
+    )
+}
+
+const isRed = true;
+
+function TrafficLight(props){
+    const isRed = props.isRed;
+    return isRed ? <Red /> : <Green />;
+}
+
+export default function Appp(){
+    return(
+        <div className={`${isRainy === true ?  'red': 'green'}`}>
+            <TrafficLight />
+
+        </div>
+    );
+}
