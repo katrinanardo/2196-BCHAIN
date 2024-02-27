b9d15788c3c1d3c475babdd42c62afaf55dbe2dc
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2024-02-27 23:47:34 +0800

    Add files via upload

---
+++ SolidityBasicsDemo/ownable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Ownable {
+
+    address public owner;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
+    }
+
+    function setOwner(address newOwner) external onlyOwner {
+        require( newOwner != address(0), "invalid address");
+        owner = newOwner;
+    }
+
+    function onlyOwnerCanCallThisFunction() external onlyOwner {
+
+    }
+
+    function anyoneCanCallThisFunction() external {
+        
+    }
+}
