05ed87ce19db4890e9e9c06fe9a848e0499e981b
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2024-04-02 13:13:48 +0800

    Add files via upload

---
+++ Finals/Lab_Activity_1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.10;
+
+contract EtherWallet {
+    event PaymentReceived(uint amount);
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    receive() external payable { 
+        emit PaymentReceived(msg.value);
+    }
+}
