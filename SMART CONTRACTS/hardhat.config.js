3e608d0c01981770fed2f8dec3af1a2605a683ed
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2024-04-07 19:06:02 +0800

    Add files via upload

---
+++ SMART CONTRACTS/hardhat.config.js
@@ -0,0 +1,26 @@
+//require("@nomicfoundation/hardhat-toolbox");
+
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+};
